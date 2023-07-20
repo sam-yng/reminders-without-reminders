7cfd2f0cd49a05c72ed45f479ebc0a4fff39488b
sam-yng <young142001@hotmail.com>
2023-07-20 15:27:56 +1000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import classNames from "classnames";
+
+export const App: React.FC = () => {
+  return (
+    <main className={classNames("h-screen")}>
+      <h1></h1>
+    </main>
+  );
+};
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      "black": "#08070B",
+      "huh": "#18171F",
+      "offblack": "#14131B",
+      "gray": "#817D92",
+      "offgray": "#24232C",
+      "altwhite": "#E6E5EA",
+      "green": "#A4FFAF"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
